22c3d3e7d19a80ccae52d8eae51d35ad8986b75e
trickybestia <trickybestia@gmail.com>
2025-11-22 11:46:14 +0300

    Add serial_data_sender to oscilloscope

---
+++ model/oscilloscope/serial_data_sender.py
@@ -0,0 +1,29 @@
+from math import sin
+from random import randint
+from sys import argv
+
+from serial import Serial
+
+
+def sample_signal(t: int) -> int:
+    return min(
+        255,
+        max(0, round((sin(t / 30) + 1) * 255 / 2) + randint(-20, 20)),
+    )
+
+
+def main():
+    port = argv[1]
+    baud_rate = int(argv[2])
+
+    serial = Serial(port, baud_rate)
+
+    t = 0
+
+    while True:
+        serial.write([sample_signal(t)])
+
+        t += 1
+
+
+main()
