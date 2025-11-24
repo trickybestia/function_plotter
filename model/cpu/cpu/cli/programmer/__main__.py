5e2edcd817a2adcba103cf08a6a18a1ee96eca7a
trickybestia <trickybestia@gmail.com>
2025-11-24 13:50:12 +0300

    Pad memory with zeros to INSTRUCTION_MEM_SIZE in cpu.cli.programmer

--- model/cpu/cpu/cli/programmer/__main__.py
+++ model/cpu/cpu/cli/programmer/__main__.py
@@ -5,18 +5,27 @@ from serial import Serial
 from cpu.utils import read_mem
 
 
+def mem_to_serial(mem: list[int], pad_to: int) -> list[int]:
+    result = [0] * pad_to
+
+    for i in range(len(mem)):
+        result[i * 2] = mem[i] >> 8
+        result[i * 2 + 1] = mem[i] & 0xFF
+
+    return result
+
+
 def main():
     port = argv[1]
     baud_rate = int(argv[2])
     mem_file_path = argv[3]
+    instr_mem_size = int(argv[4])
+
+    mem = read_mem(mem_file_path)
 
     serial = Serial(port, baud_rate)
 
-    for word in read_mem(mem_file_path):
-        msb = word >> 8
-        lsb = word & 0xFF
-
-        serial.write([msb, lsb])
+    serial.write(mem_to_serial(mem, instr_mem_size))
 
 
 main()
